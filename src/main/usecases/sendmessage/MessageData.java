5ccfb6d210c0320c01689727ba5466d312b89cda
Frederick Meneses <75036259+avinight@users.noreply.github.com>
2022-12-19 18:01:00 -0500

    Initial Setup

--- src/main/usecases/sendmessage/MessageData.java
+++ src/main/usecases/sendmessage/MessageData.java
@@ -1,4 +1,14 @@
 package main.usecases.sendmessage;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
 public class MessageData {
+    private String text;
 }
